87b62745b7111fd9a94cc3fc235dba97a5c6d02b
AlexMinez <alex_minihan@outlook.com>
2022-03-11 13:25:42 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+
+
+let employee = {
+    employeeName: 'Greg',
+    employeeAddress: '123 Fake Street'
+}
+function updateEmployeeWithKeyAndValue(employee, employeeName, employeeAddress){
+    const newObj = {...employee};
+    newObj[employeeName]=employeeAddress
+    return newObj 
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, employeeName, employeeAddress){
+    employee[employeeName]= employeeAddress;
+    return employee
+}
+function deleteFromEmployeeByKey(employee){
+    const newObj = {...employee}
+    delete newObj.employeeName
+    return newObj.employeeName = { employeeName: 'Sam'}
+}
+function destructivelyDeleteFromEmployeeByKey(employee, employeeName){
+    
+   Reflect.deleteProperty(employee,employeeName)
+   return employee
+}
